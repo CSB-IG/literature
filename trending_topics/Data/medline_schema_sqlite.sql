138cb4e9299e570ffddf48a20e5d08b6b428b332
absorto <rgarcia@inmegen.gob.mx>
2013-04-30 18:53:23 +0000

    correct diagram, run parsediasql to generate schema.sql
    git-svn-id: svn+ssh://svn.code.sf.net/p/caopsci/code-0/trunk@35 b2fe6fb4-2452-49cb-8daa-292c37fa37fb

--- trending_topics/Data/medline_schema_sqlite.sql
+++ trending_topics/Data/medline_schema_sqlite.sql
@@ -4,7 +4,7 @@
 -- Architecture          x86_64-linux-gnu-thread-multi             
 -- Target Database       sqlite3fk                                 
 -- Input file            medline.dia                               
--- Generated at          Fri Apr 26 15:24:54 2013                  
+-- Generated at          Tue Apr 30 13:45:02 2013                  
 -- Typemap for sqlite3fk not found in input file                   
 
 -- get_constraints_drop 
@@ -25,13 +25,15 @@ drop table if exists citation_journal;
 drop table if exists citation_author;
 drop table if exists citation_history;
 drop table if exists citation_lang;
+drop table if exists mesh_subheading;
+drop table if exists mesh_citation;
 
 -- get_smallpackage_pre_sql 
 
 -- get_schema_create
 
 create table citation (
-   pmid                        int                   ,
+   pmid                        INTEGER PRIMARY KEY,
    date_created                date                  ,
    date_completed              date                  ,
    date_revised                date                  ,
@@ -41,87 +43,97 @@ create table citation (
    abstract                    varchar(4000)         ,
    publication_type            varchar(200)          ,
    pagination                  varchar(200)          ,
-   copyright_information       varchar(2000)         ,
-   
+   copyright_information       varchar(2000)         
 )   ;
 
 create table journal (
-   journal_id       int           ,
-   issn             varchar(30)   ,
-   volume           varchar(200)  ,
-   issue            varchar(200)  ,
-   pub_date         date          ,
-   title            varchar(2000) ,
-   iso_abbreviation varchar(50)   ,
-   country          varchar(50)   ,
-   
+   journal_id       INTEGER PRIMARY KEY,
+   issn             varchar(30)           ,
+   volume           varchar(200)          ,
+   issue            varchar(200)          ,
+   pub_date         date                  ,
+   title            varchar(2000)         ,
+   iso_abbreviation varchar(50)           ,
+   country          varchar(50)           
 )   ;
 
 create table language (
-   language_id int         ,
-   language    varchar(50) ,
-   
+   language_id INTEGER PRIMARY KEY,
+   language    varchar(50)         
 )   ;
 
 create table publication_history_status (
-   phst_id int                 ,
+   phst_id INTEGER PRIMARY KEY,
    status  varchar(50)         ,
    date    date                ,
-   pmid    int         not null,
-   
+   pmid    int         not null
 )   ;
 
 create table author (
-   author_id INTEGER PRIMARY KEY ,
-   name      varchar(450)        ,
-   full_name varchar(500)        ,
-   
+   author_id INTEGER PRIMARY KEY,
+   name      varchar(450)         ,
+   full_name varchar(500)         
 )   ;
 
 create table subheading (
-   id    int     ,
-   term  varchar ,
-   major bool    ,
-   
+   id    INTEGER PRIMARY KEY,
+   term  varchar         ,
+   major bool            
 )   ;
 
 create table meshterm (
-   id    int     ,
-   term  varchar ,
-   major bool    ,
-   
+   id    int     INTEGER PRIMARY KEY,
+   term  varchar         ,
+   major bool            ,
+   other bool            
 )   ;
 
 create table citation_journal (
-   pmid       int ,
-   journal_id int ,
+   pmid       int not null,
+   journal_id int not null,
    constraint pk_citation_journal primary key (pmid,journal_id),
    foreign key(pmid) references citation(pmid) on delete cascade,
    foreign key(journal_id) references journal(journal_id) on delete cascade
 )   ;
 
 create table citation_author (
-   pmid      int                 ,
-   author_id INTEGER PRIMARY KEY ,
+   pmid      int not null,
+   author_id int not null,
    constraint pk_citation_author primary key (pmid,author_id),
    foreign key(pmid) references citation(pmid) on delete cascade,
    foreign key(author_id) references author(author_id) on delete cascade
 )   ;
 
 create table citation_history (
-   pmid    int ,
-   phst_id int ,
+   pmid    int not null,
+   phst_id int not null,
    constraint pk_citation_history primary key (pmid,phst_id),
    foreign key(pmid) references citation(pmid) on delete cascade,
    foreign key(phst_id) references publication_history_status(phst_id) on delete cascade
 )   ;
 
 create table citation_lang (
-   pmid        int ,
-   language_id int ,
+   pmid        int not null,
+   language_id int not null,
    constraint pk_citation_lang primary key (pmid,language_id),
    foreign key(pmid) references citation(pmid) on delete cascade,
    foreign key(language_id) references language(language_id) on delete cascade
+)   ;
+
+create table mesh_subheading (
+   msh_id int not null,
+   sbh_id int not null,
+   constraint pk_mesh_subheading primary key (msh_id,sbh_id),
+   foreign key(msh_id) references meshterm(id) on delete cascade,
+   foreign key(sbh_id) references subheading(id) on delete cascade
+)   ;
+
+create table mesh_citation (
+   pmid int not null,
+   id   int not null,
+   constraint pk_mesh_citation primary key (pmid,id),
+   foreign key(pmid) references citation(pmid) on delete cascade,
+   foreign key(id) references meshterm(id) on delete cascade
 )   ;
 
 -- get_view_create
