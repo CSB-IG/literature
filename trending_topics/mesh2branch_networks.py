0463b8d6b6f0ebf7980e815a685a2b2b1129e652
absorto <rgarcia@inmegen.gob.mx>
2013-07-01 19:49:06 +0000

    reduce meshterms to their root, construct network from that, dump it to csv
    git-svn-id: svn+ssh://svn.code.sf.net/p/caopsci/code-0/trunk@59 b2fe6fb4-2452-49cb-8daa-292c37fa37fb

---
+++ trending_topics/mesh2branch_networks.py
@@ -0,0 +1,44 @@
+from elixir import *
+from models.medline import *
+import time, datetime
+import pprint
+import networkx as nx
+
+metadata.bind = 'mysql+oursql://caopsci:G@localhost/medline'
+
+setup_all()
+
+
+
+
+
+for year in range(1987,2014):
+    start = datetime.datetime(year,1,1)
+    end   = datetime.datetime(year+1,1,1)
+
+    year_query = Citation.query.filter(Citation.date_created>=start,
+                                       Citation.date_created<=end)
+    G = nx.Graph()
+
+    for cit in year_query.all():
+        try:
+            for mh_source in cit.meshterms:
+                for mh_target in cit.meshterms:
+                    for branch_s in mh_source.term.branches:
+                        key_s = branch_s.branch[:1]
+                        for branch_t in mh_target.term.branches:
+                            key_t = branch_t.branch[:1]
+                            if key_s != key_t:
+                                e = G.get_edge_data(key_s, key_t)
+                                if not e:
+                                    G.add_edge(key_s, key_t, weight=1)
+                                else:
+                                    G.add_edge(key_s, key_t, weight=e['weight']+1)
+                                
+        except:
+            pass
+
+
+    with open('branch_network_'+str(year)+'.csv', 'w') as f:
+        for e in G.edges():
+            f.write("%s,%s,%s\n" % (e[0], e[1], G.get_edge_data(e[0], e[1])['weight']) )
