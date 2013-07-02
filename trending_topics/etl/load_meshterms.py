934ae5694e7f84f6cace8720df88da36e7150cb7
absorto <rgarcia@inmegen.gob.mx>
2013-07-02 21:47:58 +0000

    rescaling code for edge weights, root categories in a comment
    git-svn-id: svn+ssh://svn.code.sf.net/p/caopsci/code-0/trunk@60 b2fe6fb4-2452-49cb-8daa-292c37fa37fb

--- trending_topics/etl/load_meshterms.py
+++ trending_topics/etl/load_meshterms.py
@@ -66,3 +66,24 @@ for line in lines:
 #         sh.merge()
 #         session.commit()
 #         print sh
+
+
+
+#####################################################################
+# A Anatomy                                                         #
+# B Organisms                                                       #
+# C Diseases                                                        #
+# D Chemical and Drugs                                              #
+# E Analytical, Diagnostic and Therapeutic Techniques and Equipment #
+# F Psychiatry and Psychology                                       #
+# G Biological Sciences                                             #
+# H Natural Sciences                                                #
+# I Anthropology, Education, Sociology and Social Phenomena         #
+# J Technology, Industry, Agriculture                               #
+# K Humanities                                                      #
+# L Information Science                                             #
+# M Named Groups                                                    #
+# N Health Care                                                     #
+# V Publication Characteristics                                     #
+# Z Geographic Locations                                            #
+#####################################################################
--- trending_topics/mesh2branch_networks.py
+++ trending_topics/mesh2branch_networks.py
@@ -3,6 +3,7 @@ from models.medline import *
 import time, datetime
 import pprint
 import networkx as nx
+import numpy
 
 metadata.bind = 'mysql+oursql://caopsci:G@localhost/medline'
 
@@ -39,6 +40,15 @@ for year in range(1987,2014):
             pass
 
 
+    weights = []
+    for e in G.edges():
+        weights.append(float(G.get_edge_data(e[0], e[1])['weight']))
+
+    arr = numpy.array(weights)
+    arr /= 2.0
+    
     with open('branch_network_'+str(year)+'.csv', 'w') as f:
         for e in G.edges():
-            f.write("%s,%s,%s\n" % (e[0], e[1], G.get_edge_data(e[0], e[1])['weight']) )
+            x = int( G.get_edge_data(e[0], e[1])['weight'] ) /2
+            z = (x/arr.max()) * 100
+            f.write("%s,%s,%s\n" % (e[0], e[1], z))
