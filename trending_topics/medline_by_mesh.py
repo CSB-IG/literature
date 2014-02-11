83b248232d2b2eb460246047a5479ae726b55f3f
jmsiqueiros <jsiqueiros@inmegen.gob.mx>
2014-02-11 21:25:36 +0000

    Busca articulos en PUBMED por un mesh-term determinado y trae los registros de los articulos en formato de medline. Tienen definido solamente traer 10 registros.
    git-svn-id: svn+ssh://svn.code.sf.net/p/caopsci/code-0/trunk@108 b2fe6fb4-2452-49cb-8daa-292c37fa37fb

---
+++ trending_topics/medline_by_mesh.py
@@ -0,0 +1,19 @@
+#Trae id de los articulos que tienen el mesh term 'genomics'. para buscar articulos con otro mesh term, sustiuir el mesh por el termino de interes en term='meshterm['MH']
+
+from Bio import Entrez
+#import cPickle, bz2
+#import os
+
+Entrez.email = 'jsiqueiros@inmegen.gob.mx'
+
+handle = ( Entrez.esearch( db="pubmed", retmax=10, term='genomics[MH]' ) )
+record = Entrez.read(handle)
+
+ids_list = record['IdList']
+
+mdl_records = ( Entrez.efetch( db="pubmed", id=ids_list, rettype='medline', retmode='text' ) )
+
+print(mdl_records.read())
+
+
+##Falta buscar la manera para que haga una busqueda cada x tiempo y no repita registros. También falta encotrar el metodo para cargar los resultados en la base de datos.
