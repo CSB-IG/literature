e4fce31ee0515b9edab7d876f97a055832125da0
jmsiqueiros <jsiqueiros@inmegen.gob.mx>
2014-03-19 13:12:43 -0600

    Code was simplefied and many lines were eliminated: loop for extracting values from dictionary and converted to sets was eliminated. A function to generate the network from a given dictionary and with a jaccard index threshold was created. Graph visualization code was poped-out from the function that generates the network. A toy dictionary was included in order to test the code.

--- jaccard/jinx_network.py
+++ jaccard/jinx_network.py
@@ -1,6 +1,6 @@
 #This is a piece of code for generating a network using as input a
 #dictionary file (dictionary.py).
-
+### TO-DO: To generate a pickle
 import networkx as nx
 import matplotlib as plt
 import itertools as itools
@@ -9,26 +9,29 @@ import itertools as itools
 def jaccard_index( first, *others ):
     return float( len ( first.intersection( *others ) ) )/ float( len( first.union( *others) ) )
 
-values = []
 
-for k in dictionary:
-    values.append( set(dictionary.get( k ) ) )
+diccionario = {
+    'a': set([1,3,5]),
+    'b': set([1,2,5]),
+    }
 
 
-G = nx.Graph()
 
-keys = []
+def network_from_dict( diccionario, threshold ):
+    G = nx.Graph()
 
-for nodes in dictionary:
-    keys.append( nodes )
-for pair in itools.combinations( keys, 2 ):
-    source = pair[0]
-    target = pair[1]
-    e = G.get_edge_data( source, target )
-    if not e:
-        G.add_edge( source, target, jin = jaccard_index( *values ) )
-    nx.draw(G)
-    pl.show()
+    for pair in itools.combinations( diccionario.keys(), 2 ):
+        source = pair[0]
+        target = pair[1]
+        jin = jaccard_index( diccionario[source], diccionario[target] )
+        if jin >= threshold:
+            G.add_edge( source, target, jin = jin)
+
+    return G
+
+h = network_from_dict( diccionario, 0.5 )
+nx.draw(h)
+pl.show()
 
 
 
