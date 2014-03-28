871c93f48168baffcbc4c21ba7d200589213ccbb
jmsiqueiros <jsiqueiros@inmegen.gob.mx>
2014-03-27 18:32:31 -0600

    Plotting instructions were modified to plot two networks in one panel

--- pruning/mi_triangles.py
+++ pruning/mi_triangles.py
@@ -2,15 +2,17 @@ import networkx as nx
 import itertools
 import matplotlib.pyplot as plt
 
+fig = plt.figure()
+fig.subplots_adjust(left=0.2, wspace=0.6)
 
 G = nx.Graph()
 
 G.add_edges_from([(1,2,{'w': 6}),
                   (2,3,{'w': 3}),
                   (3,1,{'w': 4}),
-                  (2,4,{'w': 2}),
-                  (4,3,{'w': 3}),
-
+                  (3,4,{'w': 12}),
+                  (4,5,{'w': 13}),
+                  (5,3,{'w': 11}),
 
               ])
 
@@ -32,14 +34,14 @@ for trio in itertools.combinations(G.nodes(), 3):
         triangles.append(vertices)
 
 pos = nx.spring_layout(G)
-nx.draw(G,
+graph1 = fig.add_subplot(121)
+graph1.plot(nx.draw(G,
         pos=pos,
         node_size  = [G.degree(n) for n in G.nodes()],
         width      = [G.get_edge_data(*e)['w'] for e in G.edges()],
-        edge_color = [G.get_edge_data(*e)['w'] for e in G.edges()] )
+        edge_color = [G.get_edge_data(*e)['w'] for e in G.edges()] ))
 
-plt.show()
-
+#plt.show()
 
 
 for t in triangles:
@@ -56,12 +58,12 @@ for t in triangles:
         quitar = l.pop()
         G.remove_edge(*weights[quitar])
 
-
-nx.draw(G,
+graph2 = fig.add_subplot(122)
+graph2.plot(nx.draw(G,
         pos=pos,
         node_size  = [G.degree(n) for n in G.nodes()],
         width      = [G.get_edge_data(*e)['w'] for e in G.edges()],
-        edge_color = [G.get_edge_data(*e)['w'] for e in G.edges()] )
+        edge_color = [G.get_edge_data(*e)['w'] for e in G.edges()] ))
 
 
 
