f22a52a83d0879476ccf418b14846c1ec7d44cc1
jmsiqueiros <jsiqueiros@inmegen.gob.mx>
2014-04-09 17:48:06 -0500

    get a list of collocated pairs of tokens

--- text_processing/collocations_extractor.py
+++ text_processing/collocations_extractor.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python
 
 import nltk
-import pprint
-import argparse
 
-raw = open('journal.pone.0066341-1.txt').read()
-token = nltk.tokenize.RegexpTokenizer(r'\w+').tokenize(raw)
-
-def text_filter( token, length, frequency, no_bigrams ):
+def extract( token_list, lengths, frequency, no_bigrams ):
     text = []
-    for w in token:
-        if len(w) > 3:
+    for w in token_list:
+        if len(w) > lengths:
             text.append(w)
-            pprint.pprint(text)
     pairs = nltk.bigrams(text)
     bigrams_measures = nltk.collocations.BigramAssocMeasures()
     trigram_measures = nltk.collocations.TrigramAssocMeasures()
     finder = nltk.collocations.BigramCollocationFinder.from_words(pairs)
     finder.apply_freq_filter( frequency )
-    print finder.nbest( bigrams_measures.pmi, no_bigrams )
+    pares =[]
+    for par in finder.nbest( bigrams_measures.pmi, no_bigrams ):
+        pares.append(par[0])
+        pares.append(par[1])
+    return pares
+
+
