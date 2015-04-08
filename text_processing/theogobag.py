a47db87e1a3a6e84ba357df4ac1cb6d9ef82f5bc
absorto <absorto@sdf.org>
2015-04-08 00:00:32 -0500

    output a pickle, better nnp detection

--- text_processing/theogobag.py
+++ text_processing/theogobag.py
@@ -9,33 +9,41 @@ from itertools import combinations
 import matplotlib.pyplot as plt
 
 
-
-def txt2tuples(text):
+# returns a dictionary, pairs of NNPs are the keys, keywords the value
+def nnps_and_keywords(text):
     s = parsetree(text, relations=True, lemmata=True)
 
-    tuples = {}
+    nnp_kw = {}
     for e in s:
         d = Document(e)
-        kw = d.keywords(top=15)
-        nnp = []    
+        kw = d.keywords()
+
+        nnp = set()
         for w in kw:
             if w[1].type == 'NNP':
-                nnp.append(w[1].string)
+                wdstr = []
+                for wd in w[1].phrase.words:
+                    if wd.type == 'NNP':
+                        wdstr.append(wd.string)
+                nnp.add("-".join(wdstr))
 
 
-        kw = d.keywords()
-        words = []
+        kw = d.keywords(top=5)
+        words = set()
         for w in kw:
-            if w[1].type != 'NNP':        
-                words.append(w[1].string)
+            if w[1].type != 'NNP':
+                if w[1].lemma:
+                    words.add(w[1].lemma)
+                else:
+                    words.add(w[1].string)
 
-        if len(nnp)>1:
-            if words:
-                tuples[tuple(nnp)]=words.pop(0)
+        if len(nnp)>1 and len(words)>1:
+            if tuple(nnp) in nnp_kw:
+                nnp_kw[tuple(nnp)].update(words)
             else:
-                tuples[tuple(nnp)]=''
+                nnp_kw[tuple(nnp)]=words
 
-    return tuples
+    return nnp_kw
 
 
 
@@ -45,13 +53,12 @@ def tuples2graph(tuples):
     for t in tuples:
         for pair in combinations(t,2):
             e = g.get_edge_data(*pair)
-
             if e:
                 bow = e['bow']
                 bow.add(tuples[t])
-                g.add_edge(*pair, bow=bow)
+                g.add_edge(pair[0], pair[1], bow=bow)
             else:
-                g.add_edge(pair[0], pair[1], bow=set([tuples[t],]))
+                g.add_edge(pair[0], pair[1], bow=tuples[t])
 
     return g
 
@@ -59,7 +66,6 @@ text = ""
 for line in fileinput.input():
     text += "\n" + line
 
+G = tuples2graph(nnps_and_keywords(text))
 
-G = tuples2graph(txt2tuples(text))
-
-nx.write_edgelist(G, sys.stdout, data=True)
+nx.write_gpickle(G, sys.stdout)
