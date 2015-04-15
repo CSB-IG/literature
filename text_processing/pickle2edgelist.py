14cb33b069337284e9ac391da8c2941b1646c694
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2015-04-14 23:58:04 -0500

    finished converter, prints to output

--- text_processing/pickle2edgelist.py
+++ text_processing/pickle2edgelist.py
@@ -1,5 +1,12 @@
-import fileinput
-import sys
 
-for e in G.edges():
-    print e[0],e[1],",".join(G.get_edge_data(*e)['bow'])
+import argparse
+import networkx as nx
+
+parser = argparse.ArgumentParser(description='Interactive view of network')
+parser.add_argument('--pickle', type=argparse.FileType('r'), required=True )
+args   = parser.parse_args()
+
+h = nx.read_gpickle( args.pickle )
+
+for e in h.edges():
+    print e[0],e[1],",".join(h.get_edge_data(*e)['bow'])
